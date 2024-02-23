32a0d90b3bee6980d58a1758217456d711e2e196
Emma Gort <2577478G@student.gla.ac.uk>
2024-02-23 12:08:02 +0000

    Updates

---
+++ precache-manifest.eaf88ad1e085a53a71b92599485f8dda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a1d3967bf4f02fe0a5d2702365ca37",
+    "url": "/Diss/index.html"
+  },
+  {
+    "revision": "15e3932af99d6b9c7c80",
+    "url": "/Diss/static/css/2.556311e9.chunk.css"
+  },
+  {
+    "revision": "cd93b6b5e811a5b0ce96",
+    "url": "/Diss/static/css/main.57723f53.chunk.css"
+  },
+  {
+    "revision": "15e3932af99d6b9c7c80",
+    "url": "/Diss/static/js/2.f781b97a.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/Diss/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "cd93b6b5e811a5b0ce96",
+    "url": "/Diss/static/js/main.f03e1ee1.chunk.js"
+  },
+  {
+    "revision": "af1193ddb120d08354ef",
+    "url": "/Diss/static/js/runtime~main.e26b1b8f.js"
+  }
+]);
