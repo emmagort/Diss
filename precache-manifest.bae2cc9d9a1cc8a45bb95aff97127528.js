31df6570b26a2c936edf39f9d4cb7838a6d55c3d
Emma Gort <2577478G@student.gla.ac.uk>
2024-02-26 15:36:32 +0000

    Updates

--- precache-manifest.bae2cc9d9a1cc8a45bb95aff97127528.js
+++ precache-manifest.bae2cc9d9a1cc8a45bb95aff97127528.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c8a1d3967bf4f02fe0a5d2702365ca37",
+    "revision": "d979bd24e46507ecaccf5b29a03e7186",
     "url": "/Diss/index.html"
   },
   {
-    "revision": "15e3932af99d6b9c7c80",
+    "revision": "876774f003e835947922",
     "url": "/Diss/static/css/2.556311e9.chunk.css"
   },
   {
-    "revision": "cd93b6b5e811a5b0ce96",
+    "revision": "bbb289d355641e600462",
     "url": "/Diss/static/css/main.57723f53.chunk.css"
   },
   {
-    "revision": "15e3932af99d6b9c7c80",
-    "url": "/Diss/static/js/2.f781b97a.chunk.js"
+    "revision": "876774f003e835947922",
+    "url": "/Diss/static/js/2.d7c1c63a.chunk.js"
   },
   {
     "revision": "0eb3ca9d0061837f6c8f",
     "url": "/Diss/static/js/3.31c88699.chunk.js"
   },
   {
-    "revision": "cd93b6b5e811a5b0ce96",
-    "url": "/Diss/static/js/main.f03e1ee1.chunk.js"
+    "revision": "bbb289d355641e600462",
+    "url": "/Diss/static/js/main.8319ad20.chunk.js"
   },
   {
     "revision": "af1193ddb120d08354ef",
